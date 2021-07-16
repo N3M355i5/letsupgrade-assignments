7cab7c1232ca5f4e1676a187ce3470775aaa76f9
N3M355i5 <aryanvarshney1@gmail.com>
2021-07-16 22:25:22 +0530

    day4assigment

--- Day 3/Assignment/tictactoe/src/App.js
+++ Day 3/Assignment/tictactoe/src/App.js
@@ -2,11 +2,11 @@ import React, { useState } from "react"
 import Icon from "./Components/icon.js"
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Button } from 'reactstrap';
+import { Button, Container, Card, CardBody, Row, Column } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import "./style.css"
 
-const tictakArray = Array(9).fill("");
+const tiktacArray = Array(9).fill("");
 const App = () => {
     let [isCross, setIsCross] = useState(true)
     let [winMessage, setWinMessage] = useState("")
@@ -14,64 +14,54 @@ const App = () => {
     const playAgain = () => {
         setIsCross(true)
         setWinMessage("")
-        tictakArray.fill("");
+        tiktacArray.fill("");
     }
 
-    const findWinner = () => {
-        if(tictakArray[0] == tictakArray[1] == tictakArray[2] == "X") {
-            setWinMessage("Winner is X")
-        }
-        if(tictakArray[0] == tictakArray[1] == tictakArray[2] == "O") {
-            setWinMessage("Winner is O")
-        }
-        if(tictakArray[3] == tictakArray[4] == tictakArray[5] == "X") {
-            setWinMessage("Winner is X")
-        }
-        if(tictakArray[3] == tictakArray[4] == tictakArray[5] == "O") {
-            setWinMessage("Winner is O")
-        }
-        if(tictakArray[6] == tictakArray[7] == tictakArray[8] == "X") {
-            setWinMessage("Winner is X")
-        }
-        if(tictakArray[6] == tictakArray[7] == tictakArray[8] == "O") {
-            setWinMessage("Winner is O")
-        }
-        if(tictakArray[0] == tictakArray[3] == tictakArray[6] == "X") {
-            setWinMessage("Winner is X")
-        }
-        if(tictakArray[0] == tictakArray[3] == tictakArray[6] == "O") {
-            setWinMessage("Winner is O")
-        }
-        if(tictakArray[1] == tictakArray[4] == tictakArray[7] == "X") {
-            setWinMessage("Winner is X")
-        }
-        if(tictakArray[1] == tictakArray[4] == tictakArray[7] == "O") {
-            setWinMessage("Winner is O")
-        }
-        if(tictakArray[2] == tictakArray[5] == tictakArray[8] == "X") {
-            setWinMessage("Winner is X")
-        }
-        if(tictakArray[2] == tictakArray[5] == tictakArray[8] == "O") {
-            setWinMessage("Winner is O")
-        }
-        if(tictakArray[0] == tictakArray[4] == tictakArray[8] == "X") {
-            setWinMessage("Winner is X")
-        }
-        if(tictakArray[2] == tictakArray[4] == tictakArray[8] == "O") {
-            setWinMessage("Winner is O")
-        }
-        if(tictakArray[2] == tictakArray[4] == tictakArray[6] == "X") {
-            setWinMessage("Winner is X")
-        }
-        if(tictakArray[2] == tictakArray[4] == tictakArray[6] == "O") {
-            setWinMessage("Winner is O")
-        }
+    const findWinner = (index) => {
+        if(tiktacArray[0]== tiktacArray[1] && tiktacArray[0]==tiktacArray[2] && tiktacArray[0]!=""){
+            setWinMessage(tiktacArray[0]+" has won")
+         }
+         else if(tiktacArray[3]== tiktacArray[4] && tiktacArray[3]==tiktacArray[5] && tiktacArray[3]!=""){
+             setWinMessage(tiktacArray[3]+" has won")
+         }
+         else if(tiktacArray[6]== tiktacArray[7] && tiktacArray[6]==tiktacArray[8] && tiktacArray[6]!=""){
+             setWinMessage(tiktacArray[6]+" has won")
+         }
+         else if(tiktacArray[0]== tiktacArray[3] && tiktacArray[0]==tiktacArray[6] && tiktacArray[0]){
+             setWinMessage(tiktacArray[0]+" has won")
+         }
+         else if(tiktacArray[1]== tiktacArray[4] && tiktacArray[1]==tiktacArray[7] && tiktacArray[1]){
+             setWinMessage(tiktacArray[1]+" has won")
+         }
+         else if(tiktacArray[2]== tiktacArray[5] && tiktacArray[2]==tiktacArray[8] && tiktacArray[2]){
+             setWinMessage(tiktacArray[2]+" has won")
+         }
+         else if(tiktacArray[2]== tiktacArray[4] && tiktacArray[2]==tiktacArray[6] && tiktacArray[2]){
+             setWinMessage(tiktacArray[2]+" has won")
+         }
+         else if(tiktacArray[0]== tiktacArray[4] && tiktacArray[0]==tiktacArray[8] && tiktacArray[0]){
+             setWinMessage(tiktacArray[0]+" has won")
+         }
+    }
 
+    const changeItem = (index) => {
+        if(winMessage) {
+            toast("Game has already got over!", {type:"success"})
+        if(tiktacArray[index] == "") {
+            tiktacArray[index] = isCross ? "cross" : "circle"
+            setIsCross(!isCross)
+        }
+        else {
+            return toast("This already occupied!", {type: "error"})
+        }
+        }
     }
 
     return (
         <div>
-            <h1><Icon choice="circle"/></h1>
+            <Container class>
+
+            </Container>
         </div>
     )
 }
