c113b7bf12e5cd7ae4a710eb9a54d0b51571d557
N3M355i5 <aryanvarshney1@gmail.com>
2021-07-16 15:16:45 +0530

    assignment 3

--- Day 3/Assignment/tictactoe/src/App.js
+++ Day 3/Assignment/tictactoe/src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import Icon from "./Components/icon.js"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Button } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import "./style.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const tictakArray = Array(9).fill("");
+const App = () => {
+    let [isCross, setIsCross] = useState(true)
+    let [winMessage, setWinMessage] = useState("")
+
+    const playAgain = () => {
+        setIsCross(true)
+        setWinMessage("")
+        tictakArray.fill("");
+    }
+
+    const findWinner = () => {
+        if(tictakArray[0] == tictakArray[1] == tictakArray[2] == "X") {
+            setWinMessage("Winner is X")
+        }
+        if(tictakArray[0] == tictakArray[1] == tictakArray[2] == "O") {
+            setWinMessage("Winner is O")
+        }
+        if(tictakArray[3] == tictakArray[4] == tictakArray[5] == "X") {
+            setWinMessage("Winner is X")
+        }
+        if(tictakArray[3] == tictakArray[4] == tictakArray[5] == "O") {
+            setWinMessage("Winner is O")
+        }
+        if(tictakArray[6] == tictakArray[7] == tictakArray[8] == "X") {
+            setWinMessage("Winner is X")
+        }
+        if(tictakArray[6] == tictakArray[7] == tictakArray[8] == "O") {
+            setWinMessage("Winner is O")
+        }
+        if(tictakArray[0] == tictakArray[3] == tictakArray[6] == "X") {
+            setWinMessage("Winner is X")
+        }
+        if(tictakArray[0] == tictakArray[3] == tictakArray[6] == "O") {
+            setWinMessage("Winner is O")
+        }
+        if(tictakArray[1] == tictakArray[4] == tictakArray[7] == "X") {
+            setWinMessage("Winner is X")
+        }
+        if(tictakArray[1] == tictakArray[4] == tictakArray[7] == "O") {
+            setWinMessage("Winner is O")
+        }
+        if(tictakArray[2] == tictakArray[5] == tictakArray[8] == "X") {
+            setWinMessage("Winner is X")
+        }
+        if(tictakArray[2] == tictakArray[5] == tictakArray[8] == "O") {
+            setWinMessage("Winner is O")
+        }
+        if(tictakArray[0] == tictakArray[4] == tictakArray[8] == "X") {
+            setWinMessage("Winner is X")
+        }
+        if(tictakArray[2] == tictakArray[4] == tictakArray[8] == "O") {
+            setWinMessage("Winner is O")
+        }
+        if(tictakArray[2] == tictakArray[4] == tictakArray[6] == "X") {
+            setWinMessage("Winner is X")
+        }
+        if(tictakArray[2] == tictakArray[4] == tictakArray[6] == "O") {
+            setWinMessage("Winner is O")
+        }
+
+    }
+
+    return (
+        <div>
+            <h1><Icon choice="circle"/></h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ Day 3/Assignment/tictactoe/src/Components/icon.js
@@ -0,0 +1,14 @@
+import  React from "react"
+import { FaTimes } from "react-icons/fa";
+import { FaRegCircle } from "react-icons/fa";
+import { FaPen } from "react-icons/fa";
+
+const Icon = ({choice}) => {
+    switch(choice) {
+        case "cross" : return <FaTimes className="icon"/>;
+        case "circle" : return <FaRegCircle className="icon"/>;
+        default : return <FaPen className="icon"/>;
+    }
+}
+
+export default Icon
